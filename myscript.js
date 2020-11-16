25ad6dd6789c9474b18542a84a883934f135ea5f
Shashikant Bharti <surya.indian321@gmail.com>
2020-11-16 19:04:41 +0530

    Decimal Added

--- myscript.js
+++ myscript.js
@@ -1,34 +1,27 @@
+// UI Elements
 let keypress = document.querySelector('.keypad');
 let display = document.querySelector('.display');
 let temp = '';
 let opr = '';
 let result = '';
+// Click Event on calculator Keys
 keypress.addEventListener('click',function(e){
-	if(e.target.className === 'num') {
-		temp += e.target.innerText;
-		display.innerText = temp;
-	} else if(e.target.className === 'opr') {
-		if(opr !== '') {
-			switch(opr) {
-			case '+': result = parseFloat(result) + parseFloat(temp); break;
-			case '-': result = parseFloat(result) - parseFloat(temp); break;
-			case '*': result = parseFloat(result) * parseFloat(temp); break;
-			case '/': result = parseFloat(result) / parseFloat(temp); break;
-			}
-		display.innerText = result;
+	debugger;
+	if(e.target.className === 'num') { // Block if number key is pressed.
+		temp += e.target.innerText; // Append number to temp variable.
+		display.innerText = temp; // Display temp in calculator display.
+	} else if(e.target.className === 'opr'  && temp !== '') { // Block if any operator is pressed.
+		if(opr !== '') { // Block if operator is empty.
+		result = calculate(opr);
+		display.innerText = result; // Display Result
 		}
-		opr = e.target.innerText;
-		if(result == '') {
+		opr = e.target.innerText; // Store new operator
+		if(result == '') { 
 			result = temp;
 		}
 		temp = '';
 	} else if(e.target.className === 'eval') {
-		switch(opr) {
-			case '+': result = parseFloat(result) + parseFloat(temp); break;
-			case '-': result = parseFloat(result) - parseFloat(temp); break;
-			case '*': result = parseFloat(result) * parseFloat(temp); break;
-			case '/': result = parseFloat(result) / parseFloat(temp); break;
-		}
+		result = calculate(opr);
 		display.innerText = result;
 		temp = '';
 		opr = '';
@@ -37,6 +30,15 @@ keypress.addEventListener('click',function(e){
 		result = '';
 		opr = '';
 		display.innerText = '';
+	}
+	function calculate(opr) {
+		switch(opr) {
+			case '+':  return parseFloat(result) + parseFloat(temp);
+			case '-':  return parseFloat(result) - parseFloat(temp);
+			case '*':  return parseFloat(result) * parseFloat(temp);
+			case '/':  return parseFloat(result) / parseFloat(temp);
+			default : return '0';
+		}
 	}
 	// console.log(temp);
 	// console.log(opr);
